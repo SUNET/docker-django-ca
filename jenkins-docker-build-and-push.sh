64f96117c1049d9cb37bdc3f8dba1b03692596f2
Johan Wassberg <jocar@sunet.se>
2025-06-25 09:48:54 +0200

    Did not compute

--- jenkins-docker-build-and-push.sh
+++ jenkins-docker-build-and-push.sh
@@ -36,8 +36,6 @@ DOCKER_TAG="${BASE_TAG}:${VERSION}"
 LATEST_TAG="${BASE_TAG}:latest"
 echo "${script_name}: building DOCKER_TAG ${DOCKER_TAG} ${LATEST_TAG}"
 
-apt update
-apt install docker-buildx-plugin
 docker version
 docker build --tag "${DOCKER_TAG}" --tag "${LATEST_TAG}" .
 #docker push --all-tags ${BASE_TAG}
